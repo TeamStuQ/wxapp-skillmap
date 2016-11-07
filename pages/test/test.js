ac3e80ddf7944d4560a0ec4e0d2f5c91074fad97
Lin Chen <linchen.1987@foxmail.com>
2016-11-07 19:57:39 +0800

    first commit

---
+++ pages/test/test.js
@@ -0,0 +1,23 @@
+Page({
+    data: {
+        name: 'linchen',
+        directory: [
+            {
+                name: "1",
+                directory: [
+                    {name: "a"},
+                    {name: "b"},
+                    {name: "c"},
+                ]
+            },
+            {
+                name: "2",
+                directory: [
+                    {name: "d"},
+                    {name: "e"},
+                    {name: "f"},
+                ]
+            }
+        ]
+    }
+});
